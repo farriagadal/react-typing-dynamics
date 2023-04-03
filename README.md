aecdaef1a86d9b56d917b9204ad2d5f5d1527bc5
Fernando <farriagadal94@gmail.com>
2023-04-03 00:48:43 -0400

    feat: add textAnimation component

---
+++ README.md
@@ -0,0 +1,60 @@
+# React Typing Dynamics
+
+A simple and customizable React component for simulating typing animations.
+
+## Installation
+
+Using npm:
+
+```bash
+npm install react-typing-dynamics
+```
+
+Using yarn:
+
+```bash
+yarn add react-typing-dynamics
+```
+
+## How to use
+Here's a basic **example** of how to use the TextAnimation component from **react-typing-dynamics**:
+
+
+```jsx
+import React from 'react';
+import TextAnimation from 'react-typing-dynamics';
+
+const App = () => {
+  const phrases = ['Hello, World!', '<h1>Welcome to my website.</h1>', 'Enjoy your stay!'];
+
+  return (
+    <TextAnimation
+      phrases={phrases}
+      typingSpeed={200}
+      backspaceDelay={50}
+      eraseDelay={1000}
+      timeComplete={3000}
+      errorProbability={0.1}
+      isSecure={true}
+    />
+  );
+};
+
+export default App;
+
+```
+## Props
+| Prop Name | Type | Description |
+| --- | --- | --- |
+| `phrases` | `string[]` | An array of phrases to be typed. Can be HTML or String |
+| `typingSpeed` | `number` | The typing speed in characters per second. |
+| `backspaceDelay` | `number` | The delay in milliseconds before the text starts to be erased. |
+| `eraseDelay` | `number` | The delay in milliseconds between each character erasing. |
+| `timeComplete` | `number` | The delay in milliseconds before the typing animation restarts. |
+| `errorProbability` | `number` | The probability (between 0 and 1) of a typing error happening. |
+| `isSecure` | `boolean` | If `true`, the text is sanitized with `sanitize-html`. |
+
+### License
+```
+This project is licensed under the MIT License - see the LICENSE file for details.
+```
---
+++ src/TextAnimation.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import sanitizeHtml from 'sanitize-html';
+import styled from 'styled-components';
+
+const Bar = styled.span`
+  &:after {
+    content: '|';
+    animation: blink 0.75s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    font-family: auto;
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+type TextAnimationProps = {
+  phrases: string[];
+  typingSpeed: number;
+  backspaceDelay: number;
+  eraseDelay: number;
+  timeComplete?: number;
+  errorProbability: number;
+  isSecure?: boolean;
+};
+
+const TextAnimation = ({
+  phrases,
+  typingSpeed,
+  backspaceDelay,
+  eraseDelay,
+  errorProbability,
+  timeComplete = 3000,
+  isSecure = true,
+}: TextAnimationProps) => {
+  const [text, setText] = useState('');
+  const [changePhrase, setChangePhrase] = useState(1);
+  const [phraseId, setPhraseId] = useState(0);
+
+  const onPhraseComplete = () => {
+    setTimeout(async () => {
+      await eraseText();
+      setTimeout(() => {
+        setChangePhrase(changePhrase + 1);
+        setPhraseId(phraseId === phrases.length - 1 ? 0 : phraseId + 1);
+      }, eraseDelay);
+    }, timeComplete);
+  };
+
+  const eraseText = () => {
+    return new Promise<void>((resolve) => {
+      let count = phrases[phraseId].length;
+      const timer = setInterval(() => {
+        setText(phrases[phraseId].slice(0, count));
+        count--;
+        if (count < 0) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, backspaceDelay);
+    });
+  };
+
+  useEffect(() => {
+    let count = 0;
+    let timer: any;
+
+    const getRandomTimeout = () => {
+      const lambda = 1 / typingSpeed;
+      const randomValue = Math.random();
+      const timeout = -Math.log(1 - randomValue) / lambda;
+      return Math.min(timeout, 200);
+    };
+
+    const animateText = () => {
+      const shouldBackspace = Math.random() < errorProbability && count > 0;
+      if (shouldBackspace) {
+        count--;
+      } else {
+        setText(phrases[phraseId].slice(0, count));
+        count++;
+      }
+      if (count === phrases[phraseId].length + 1) {
+        onPhraseComplete();
+        clearInterval(timer);
+      } else {
+        const timeout = getRandomTimeout();
+        timer = setTimeout(animateText, timeout);
+      }
+    };
+
+    timer = setTimeout(animateText, 100);
+    return () => clearTimeout(timer);
+  }, [changePhrase]);
+
+  return (
+    <Bar
+      style={{ fontFamily: 'inherit' }}
+      dangerouslySetInnerHTML={{
+        __html: isSecure ? sanitizeHtml(text) : text,
+      }}
+    />
+  );
+};
+
+export default TextAnimation;
